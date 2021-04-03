b072cda5e71c6e513f225d301d32a56028350e37
Gopinath Danda <gopinath.danda@gmail.com>
2021-04-03 00:30:18 -0700

    Update gradle. Add Toasts to buttons. Update theme.

--- app/src/main/java/com/nkumari/autohome/MainActivity.java
+++ app/src/main/java/com/nkumari/autohome/MainActivity.java
@@ -3,12 +3,24 @@ package com.nkumari.autohome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mOn;
+    Button mOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mOn = findViewById(R.id.turnOn);
+        mOff = findViewById(R.id.turnOff);
+
+        mOn.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Light turned ON!", Toast.LENGTH_SHORT).show());
+        mOff.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Light turned OFF!", Toast.LENGTH_SHORT).show());
     }
 }
